c712439aa773e2a06a3d3644308b3bc57f2d4b74
Imre Chroncsik <Imre.Chroncsik@logmein.com>
2019-01-19 18:15:01 +0100

    Fixed some build errors on Mac.

--- Source/JavascriptUMG/JavascriptToolbarButtonContext.cpp
+++ Source/JavascriptUMG/JavascriptToolbarButtonContext.cpp
@@ -17,7 +17,8 @@ FText UJavascriptToolbarButtonContext::Public_OnGetLabel()
 
 void UJavascriptToolbarButtonContext::Public_OnExecuteAction(UObject* EditingObject)
 {
-	OnExecuteAction.IsBound() ? OnExecuteAction.Execute(EditingObject) : nullptr;
+	if (OnExecuteAction.IsBound())
+		OnExecuteAction.Execute(EditingObject);
 }
 
 bool UJavascriptToolbarButtonContext::Public_OnCanExecuteAction(UObject* EditingObject)
