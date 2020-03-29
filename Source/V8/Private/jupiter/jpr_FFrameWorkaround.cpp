c47c69a1718a26cf8239142dd3aa148df37bc3a2
Imre Chroncsik <imre.chroncsik@gmail.com>
2020-03-29 07:38:27 +0200

    added comments to FFrameWorkaround

--- Source/V8/Private/jupiter/jpr_FFrameWorkaround.cpp
+++ Source/V8/Private/jupiter/jpr_FFrameWorkaround.cpp
@@ -1,4 +1,20 @@
 
+/*
+	Workaround for link errors on MacOS with UE 4.24
+
+		[2/8] Link UE4Editor-V8-Mac-DebugGame.dylib
+		Undefined symbols for architecture x86_64:
+			"vtable for FFrame", referenced from:
+				FFrame::FFrame(UObject*, UFunction*, void*, FFrame*, UField*) in Module.V8.cpp.o
+				FFrame::~FFrame() in Module.V8.cpp.o
+			NOTE: a missing vtable usually means the first non-inline virtual member function has no definition.
+
+	According to Stack.h, FFrame only has two virtuals: its destructor (implemented in the header),
+	and Serialize(). Theoretically Serialize() is implemented in ScriptCore.cpp, but somehow
+	it doesn't make it to the binary in my case.
+	As a temporary workaround, FFrame::Serialize() is duplicated here,
+	this seems to fix the error.
+*/
 
 DEFINE_LOG_CATEGORY_STATIC(LogScriptCore, Log, All);
 
@@ -44,4 +60,3 @@ void FFrame::Serialize( const TCHAR* V, ELogVerbosity::Type Verbosity, const cla
 #endif
 	}
 }
-
